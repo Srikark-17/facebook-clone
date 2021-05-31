93ad9396c7bf674c5db7acbd2164c800589ca6d9
Srikark-17 <srikar.sgk@gmail.com>
2021-05-31 17:52:46 -0500

    Finished Header and set up FB auth

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ module.exports = {
       "cdn0.iconfinder.com",
       "platform-lookaside.fbsbx.com",
       "firebasestorage.googleapis.com",
+      "1000logos.net",
+      "links.papareact.com",
+      "scontent-atl3-1.xx.fbcdn.net",
     ],
   },
 };
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Head from "next/head";
-import Header from "../components/Header";
+import { getSession } from "next-auth/client";
+import Header from "../components/Header/Header";
+import Login from "../components/Login/Login";
+import Sidebar from "../components/Sidebar/Sidebar";
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />;
   return (
     <div>
       <Head>
@@ -15,9 +19,21 @@ export default function Home() {
       <Header />
       <main>
         {/* Sidebar */}
+        <Sidebar />
         {/* Feed */}
         {/* Widgets */}
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  // Get User
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }
