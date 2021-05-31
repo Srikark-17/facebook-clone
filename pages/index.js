7783097d17032cdcf4875ea86d030c63d9f35334
Srikark-17 <srikar.sgk@gmail.com>
2021-05-31 13:33:59 -0500

    Initialized build and set up Tailwind

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Facebook</title>
+        <meta
+          name="description"
+          content="Facebook Clone created by Srikar Kusumanchi"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* Header */}
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </main>
+    </div>
+  );
+}
