8c94fe604b9f9296b35313e515063fc4ae9ea2c8
Srikark-17 <srikar.sgk@gmail.com>
2021-05-31 14:00:24 -0500

    Fixed Tailwind

---
+++ components/Header.js
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from "@heroicons/react/solid";
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+
+function Header() {
+  return (
+    <div>
+      {/* Left */}
+      <div className="flex items-center">
+        <Image
+          src="https://cdn0.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-facebook-2019-circle-512.png"
+          width={40}
+          height={40}
+          layout="fixed"
+        />
+        <div className="flex ml-2 items-center rounded-full">
+          <SearchIcon className="h-6" />
+          <input type="text" placeholder="Search Facebook" />
+        </div>
+      </div>
+      {/* Center */}
+
+      {/* Right */}
+    </div>
+  );
+}
+
+export default Header;
