2b33b8f0942f48d253775559d3bc297288d09243
Srikark-17 <srikar.sgk@gmail.com>
2021-05-31 19:59:37 -0500

    Modified Favicon

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,28 @@ export default function Home({ session, posts }) {
   return (
     <div className="h-screen bg-gray-100 overflow-hidden">
       <Head>
-        <title>Facebook</title>
+        <title>Facebook Clone</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="./apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="./favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="./favicon-16x16.png"
+        />
+        <meta
+          name="description"
+          content="Facebook Clone created by Srikar Kusumanchi"
+        />
       </Head>
 
       <Header />
