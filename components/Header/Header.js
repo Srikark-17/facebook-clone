e07e11f0fce0b3643324804496d0ac1253d6a792
Srikark-17 <srikar.sgk@gmail.com>
2021-05-31 22:51:22 -0500

    Minor CSS change

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -24,7 +24,7 @@ function Header() {
       {/* Left */}
       <div className="flex items-center">
         <Image
-          src="https://links.papareact.com/5me"
+          src="https://cdn0.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-facebook-2019-circle-512.png"
           width="40"
           height="40"
           layout="fixed"
